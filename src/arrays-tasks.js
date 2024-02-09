880c5e8a401468fb3ff9533b36bbc8f73797b964
Darya Belousova <belousmind@gmail.com>
2024-02-09 16:05:44 +0100

    feat: solve propagateItemsByPositionIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -509,10 +509,13 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
-}
 
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((total, item, index) => {
+    return total.concat(Array(index + 1).fill(item));
+  }, []);
+}
+// console.log(propagateItemsByPositionIndex([ 1,2,3,4,5 ]))
 /**
  * Shifts an array by n positions. If n is negative, the array is shifted to the left;
  * if positive, it is shifted to the right.
